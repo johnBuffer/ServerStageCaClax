a7d87dfd419ed03f8aa7673517ee405488f0f523
johnBuffer <jean.tampon@gmail.com>
2016-07-26 13:28:39 +0200

    Added utils class to handle creepy functions

---
+++ ServerTest/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTest
+{
+    class Utils
+    {
+        public static string ArrayToString(string[] array, int firstIndex, int lastIndex, bool quotation=false)
+        {
+            string result  = "";
+            for (int i = firstIndex; i < lastIndex; i++)
+            {
+                if (quotation)
+                    result += "'" + array[i] + "', ";
+                else
+                    result += array[i] + ", ";
+            }
+            result = result.Remove(result.Length - 2, 2);
+
+            return result;
+        }
+    }
+}
